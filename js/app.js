9846c413ff031d36e4a8996d2c6f3a4f41de0ee2
ZeinaAyman <56726381+ZeinaAyman@users.noreply.github.com>
2021-05-18 01:35:33 +0200

    Adding Active class and color for section and navigation links

--- js/app.js
+++ js/app.js
@@ -20,6 +20,7 @@
 const sections = document.querySelectorAll('section');
 const nav = document.querySelector('#navbar__list');
 const fragment = document.createDocumentFragment();
+const color = ["#051440","#27445c","#3a5e6b","#588884"];
 
 /**
  * End Global Variables
@@ -39,12 +40,14 @@ const fragment = document.createDocumentFragment();
 function navigationBar(){
 
   for(let i = 0; i < sections.length; i++){
+    //adding section links to the navigation bar
     var list = document.createElement('li');
     list.classList.add("menu__link");
-    var sectionName = sections[i].dataset.nav;
-    list.innerHTML = sectionName;
+    var sectionNumber = sections[i].dataset.nav;
+    list.innerHTML = sectionNumber;
     fragment.appendChild(list);
 
+    //Scroll to section on link click
     list.addEventListener("click", function() {
       sections[i].scrollIntoView({behavior: "smooth"});
     });
@@ -54,7 +57,31 @@ function navigationBar(){
 }
 
 // Add class 'active' to section when near top of viewport
-
+function addActive(){
+  window.addEventListener("scroll", function() {
+    const lists = document.getElementsByClassName('menu__link');
+    for(let i=0; i< sections.length; i++){
+      //getBoundingClientRect returns the size of the element and its position relative to the viewport.
+      boundRect = sections[i].getBoundingClientRect();
+      //if condition to assure that the top position is within the section size and hasn't gone to another section.
+      if(boundRect.top <= 300 && boundRect.top >= 0){
+        //change background color of section container.
+        sections[i].style.background=color[i];
+        //change background color of section link.
+        lists[i].style.background = color[i];
+        sections[i].classList.add("your-active-class");
+        lists[i].classList.add("active");
+      }
+      else{
+        //inherit the same gradient background color of the parent (body).
+        sections[i].style.background="inherit";
+        lists[i].style.background = "inherit";
+        sections[i].classList.remove("your-active-class");
+        lists[i].classList.remove("active");
+      }
+    }
+  });
+}
 
 // Scroll to anchor ID using scrollTO event
 
@@ -70,3 +97,4 @@ navigationBar();
 // Scroll to section on link click
 
 // Set sections as active
+addActive();
